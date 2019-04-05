a8a18c3d63587723d520af8425a4ed091e82f438
Chenbo Yi <yichenbo@outlook.com>
2019-04-05 23:04:56 +0100

    Add test for list titles

--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -2,6 +2,15 @@ require 'notes'
 
 describe Notes do
   describe '#add_note' do
-  it { is_expected.to respond_to(:add_note).with(2).arguments }
+    it { is_expected.to respond_to(:add_note).with(2).arguments }
+  end
+
+  describe '#list' do
+    it "list the titles" do
+      note = describe_class.new
+      note.add_note("title1","body1")
+      note.add_note("title2","body2")
+      expect(note.list).to eq ["title1", "title2"]
+    end
   end
 end
